258a57a9b55e2a661c38b0bec46ebb0ba47e5a97
User <he1ex-tg@yandex.ru>
2022-08-17 13:43:22 +0500

    t387, t802

---
+++ src/test/kotlin/EasyTest.kt
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class EasyTest() {
+
+    private val easy: Easy = Easy()
+
+    @Test
+    fun t387() {
+        val t1 = easy.t387("leetcode")
+        assertEquals(t1, 0)
+
+        val t2 = easy.t387("loveleetcode")
+        assertEquals(t2, 2)
+
+        val t3 = easy.t387("aabb")
+        assertEquals(t3, -1)
+    }
+
+    @Test
+    fun t804() {
+        val arr1 = easy.t804(arrayOf("gin","zen","gig","msg"))
+        assertEquals(arr1, 2)
+
+        val arr2 = easy.t804(arrayOf("a"))
+        assertEquals(arr2, 1)
+    }
+}
